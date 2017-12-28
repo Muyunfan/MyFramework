76f2df92a93b41d543ddddfac5f6d8a6c60001b6
Muyunfan <964013025@qq.com>
2017-12-28 15:41:57 +0800

    Merge remote-tracking branch 'remotes/origin/master'
    Conflicts:
    	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/muyunfan/fw/widget/utils/common/EmojiFilter.java
+++ app/src/main/java/com/muyunfan/fw/widget/utils/common/EmojiFilter.java
@@ -1,87 +1,160 @@
 package com.muyunfan.fw.widget.utils.common;
 
-import com.muyunfan.fw.widget.utils.common.CheckUtil;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.util.Log;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /*
  * 类描述：
  * 创建人：李程伟
  * 创建日期： 2017年5月25日14:44:48
  */
-public class EmojiFilter {
-    /**
-     * 检测是否有emoji字符
-     *
-     * @param source
-     * @return 一旦含有就抛出
-     */
-    public static boolean containsEmoji(String source) {
-        if (CheckUtil.isEmpty(source)) {
-            return false;
-        }
-
-        int len = source.length();
-
-        for (int i = 0; i < len; i++) {
-            char codePoint = source.charAt(i);
-
-            if (isEmojiCharacter(codePoint)) {
-                //do nothing，判断到了这里表明，确认有表情字符
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private static boolean isEmojiCharacter(char codePoint) {
-        return (codePoint == 0x0) ||
-                (codePoint == 0x9) ||
-                (codePoint == 0xA) ||
-                (codePoint == 0xD) ||
-                ((codePoint >= 0x20) && (codePoint <= 0xD7FF)) ||
-                ((codePoint >= 0xE000) && (codePoint <= 0xFFFD)) ||
-                ((codePoint >= 0x10000) && (codePoint <= 0x10FFFF));
-    }
+public class EmojiFilter implements InputFilter {
+    private static Set<String> filterSet = null;
+    private static Set<Scope> scopeSet = null;
 
     /**
-     * 过滤emoji 或者 其他非文字类型的字符
-     *
-     * @param source
-     * @return
+     * 区间类模型
      */
-    public static String filterEmoji(String source) {
+    private class Scope {
+        int start;
+        int end;
 
-        if (!containsEmoji(source)) {
-            return source;//如果不包含，直接返回
-        }
-        //到这里铁定包含
-        StringBuilder buf = null;
-
-        int len = source.length();
-
-        for (int i = 0; i < len; i++) {
-            char codePoint = source.charAt(i);
-
-            if (isEmojiCharacter(codePoint)) {
-                if (buf == null) {
-                    buf = new StringBuilder(source.length());
+        @Override
+        public boolean equals(Object o) {
+            if (o instanceof Scope) {
+                Scope scope = (Scope) o;
+                if (scope.start == start && scope.end == end) {
+                    return true;
                 }
-                buf.append(codePoint);
-            } else {
-
             }
+            return super.equals(o);
+        }
+    }
+
+    private static void addUnicodeRangeToSet(Set<String> set, int start, int end) {
+        if (set == null) {
+            return;
+        }
+        if (start > end) {
+            return;
         }
 
-        if (buf == null) {
-            return source;//如果没有找到 emoji表情，则返回源字符串
-        } else {
-            if (buf.length() == len) {//这里的意义在于尽可能少的toString，因为会重新生成字符串
-                buf = null;
-                return source;
-            } else {
-                return buf.toString();
-            }
-        }
 
+        for (int i = start; i <= end; i++) {
+            filterSet.add(new String(new int[]{i}, 0, 1));
+        }
+    }
+
+    private static void addUnicodeRangeToSet(Set<String> set, int code) {
+        if (set == null) {
+            return;
+        }
+        filterSet.add(new String(new int[]{code}, 0, 1));
+    }
+
+    static {
+        filterSet = new HashSet<String>();
+        scopeSet = new HashSet<>();
+
+        // See http://apps.timwhitlock.info/emoji/tables/unicode
+
+        // 1. Emoticons ( 1F601 - 1F64F )
+        addUnicodeRangeToSet(filterSet, 0x1F601, 0X1F64F);
+
+        // 2. Dingbats ( 2702 - 27B0 )
+        addUnicodeRangeToSet(filterSet, 0x2702, 0X27B0);
+
+        // 3. Transport and map symbols ( 1F680 - 1F6C0 )
+        addUnicodeRangeToSet(filterSet, 0X1F680, 0X1F6C0);
+
+        // 4. Enclosed characters ( 24C2 - 1F251 )
+        addUnicodeRangeToSet(filterSet, 0X24C2);
+        addUnicodeRangeToSet(filterSet, 0X1F170, 0X1F251);
+
+        // 6a. Additional emoticons ( 1F600 - 1F636 )
+        addUnicodeRangeToSet(filterSet, 0X1F600, 0X1F636);
+
+        // 6b. Additional transport and map symbols ( 1F681 - 1F6C5 )
+        addUnicodeRangeToSet(filterSet, 0X1F681, 0X1F6C5);
+
+        // 6c. Other additional symbols ( 1F30D - 1F567 )
+        addUnicodeRangeToSet(filterSet, 0X1F30D, 0X1F567);
+
+        // 5. Uncategorized
+        addUnicodeRangeToSet(filterSet, 0X1F004);
+        addUnicodeRangeToSet(filterSet, 0X1F0CF);
+        // 与6c. Other additional symbols ( 1F30D - 1F567 )重复
+        // 去掉重复部分虽然不去掉HashSet也不会重复，原范围（0X1F300 - 0X1F5FF）
+        addUnicodeRangeToSet(filterSet, 0X1F300, 0X1F30D);
+        addUnicodeRangeToSet(filterSet, 0X1F5FB, 0X1F5FF);
+        addUnicodeRangeToSet(filterSet, 0X00A9);
+        addUnicodeRangeToSet(filterSet, 0X00AE);
+        addUnicodeRangeToSet(filterSet, 0X0023);
+        //阿拉伯数字0-9，配合0X20E3使用
+        //addUnicodeRangeToSet(filterSet, 0X0030, 0X0039);
+        // 过滤掉203C开始后的2XXX 段落
+        //addUnicodeRangeToSet(filterSet, 0X203C, 0X24C2);
+        addUnicodeRangeToSet(filterSet, 0X203C);
+        addUnicodeRangeToSet(filterSet, 0X2049);
+        //严格验证的话需要判断前面是否是数字
+        //Android上显示和数字分开可以不判断
+        addUnicodeRangeToSet(filterSet, 0X20E3);
+        addUnicodeRangeToSet(filterSet, 0X2122);
+        addUnicodeRangeToSet(filterSet, 0X2139);
+        addUnicodeRangeToSet(filterSet, 0X2194, 0X2199);
+        addUnicodeRangeToSet(filterSet, 0X21A9, 0X21AA);
+        addUnicodeRangeToSet(filterSet, 0X231A, 0X231B);
+        addUnicodeRangeToSet(filterSet, 0X23E9, 0X23EC);
+        addUnicodeRangeToSet(filterSet, 0X23F0);
+        addUnicodeRangeToSet(filterSet, 0X23F3);
+        addUnicodeRangeToSet(filterSet, 0X25AA, 0X25AB);
+        addUnicodeRangeToSet(filterSet, 0X25FB, 0X25FE);
+        //TODO： 26XX 太杂全部过滤
+        addUnicodeRangeToSet(filterSet, 0X2600, 0X26FE);
+        addUnicodeRangeToSet(filterSet, 0X2934, 0X2935);
+        addUnicodeRangeToSet(filterSet, 0X2B05, 0X2B07);
+        addUnicodeRangeToSet(filterSet, 0X2B1B, 0X2B1C);
+        addUnicodeRangeToSet(filterSet, 0X2B50);
+        addUnicodeRangeToSet(filterSet, 0X2B55);
+        addUnicodeRangeToSet(filterSet, 0X3030);
+        addUnicodeRangeToSet(filterSet, 0X303D);
+        addUnicodeRangeToSet(filterSet, 0X3297);
+        addUnicodeRangeToSet(filterSet, 0X3299);
+    }
+
+    public EmojiFilter() {
+        super();
+    }
+
+    @Override
+    public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart,
+                               int dend) {
+        // check black-list set
+        for (int i = 0; i < source.length(); i++) {
+            Log.e("字符", "" + Integer.toHexString(source.charAt(i)));
+        }
+        Log.e("字符", "" + source.toString() + " length： " + source.toString().length() +
+                " ；bytes length： " + source.toString().getBytes().length);
+//        Iterator<String> iterator = filterSet.iterator();
+//        while (iterator.hasNext()) {
+//            String filter = iterator.next();
+//            if (filter.equals(source.toString())) {
+//                LogUtil.e(filter + " length： " + filter.length() +
+//                        " ；bytes length： " + filter.getBytes().length);
+//                for (int i= 0; i < source.length(); i++){
+//                    LogUtil.e(Integer.toHexString(source.charAt(i)));
+//                }
+//                return "";
+//            }
+//        }
+        if (filterSet.contains(source.toString())) {
+            ToastUtil.showShort("不支持Emoji表情！");
+            return "";
+        }
+        return source;
     }
 }
