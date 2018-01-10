bc4e2626a41595a8b3edf2d965ce99594d9d9d5d
Muyunfan <964013025@qq.com>
2018-01-10 17:30:56 +0800

    Merge remote-tracking branch 'remotes/origin/master'
    Conflicts:
    	app/src/main/AndroidManifest.xml

--- myfCore/src/main/java/com/muyunfan/fw/codermodule/widget/MyView.java
+++ myfCore/src/main/java/com/muyunfan/fw/codermodule/widget/MyView.java
@@ -6,7 +6,11 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+import android.view.Window;
+
+import com.muyunfan.fw.widget.utils.mobile.WindowUtil;
 
 /**
  * 类名称：MyView
@@ -18,16 +22,23 @@ import android.view.View;
  * 修改备注：
  */
 public class MyView extends View {
+
+    private Context context;
+    private static final String TAG = "MyView";
+
     public MyView(Context context) {
         super(context);
+        this.context = context;
     }
 
     public MyView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        this.context = context;
     }
 
     public MyView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        this.context = context;
     }
 
     Paint paint = new Paint();
@@ -51,9 +62,38 @@ public class MyView extends View {
 
         initPaint();
 
+        WindowUtil windowUtil = new WindowUtil(context);
+        int[] windowPixels = windowUtil.getWindowPixels();
+        int width = windowPixels[0];
+        int height = windowPixels[1];
+
+        Log.i(TAG, "width:" + width + " , height:" + height);
+
         //create a circle
-        canvas.drawCircle(400, 400, 200, paint);
-        canvas.drawColor(Color.parseColor("#884d4f53"));
+        canvas.drawCircle(width / 2, height / 2 - 100, 200, paint);
+
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawRect(100, 100, 500, 500, paint);
+
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawRect(600, 100, 1000, 500, paint);
+
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        paint.setStrokeWidth(30);
+        canvas.drawPoint(width / 2 - 50, (float) (height / 1.5), paint);
+
+        paint.setStrokeCap(Paint.Cap.SQUARE);
+        paint.setStrokeWidth(30);
+        canvas.drawPoint(width / 2 + 50, (float) (height / 1.5), paint);
+
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        paint.setStrokeWidth(30);
+        canvas.drawPoint(width / 2, (float) (height / 1.5 + 100), paint);
+
+        float[] points = {0, 0, 50, 50, 50, 100, 100, 50, 100, 100, 150, 50, 150, 100};
+// 绘制四个点：(50, 50) (50, 100) (100, 50) (100, 100)
+        canvas.drawPoints(points, 2 /* 跳过两个数，即前两个 0 */,
+                8 /* 一共绘制 8 个数（4 个点）*/, paint);
 
     }
 }
